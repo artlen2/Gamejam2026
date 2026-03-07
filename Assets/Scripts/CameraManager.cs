63cb319a522c3537ccfc302beecfd71fd5079f6f
Lou Malcles <2333028@cgmatane.qc.ca>
2026-03-07 10:02:42 -0500

    Scripts du joueur

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private Vector3 _offset;
+    [SerializeField] private float smoothTime;
+    private Vector3 _currentVelocity = Vector3.zero;
+
+    private void Awake()
+    {
+        _offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        var targetPosition = target.position + _offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothTime);
+    }
+}
